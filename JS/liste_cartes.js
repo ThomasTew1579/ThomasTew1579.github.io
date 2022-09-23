9ffb58e465b7d97d4da33f80d88ecbf660f5aa23
Thomas <pasquet.thomas69@gmail.com>
2022-09-23 17:16:44 +0200

    test proto-galerie video

--- JS/liste_cartes.js
+++ JS/liste_cartes.js
@@ -271,3 +271,21 @@ document.querySelector('.C1').addEventListener("click", () => {
 // ----------------------------------------------------annimation carte section
 
 // =================================================evenement page liste cartes
+
+
+//Animation galerie ============================================================
+
+let angle = 0;
+function galleryspin(sign) { 
+spinner = document.querySelector("#spinner");
+if (!sign) { 
+  angle = angle + 45; 
+  } else { 
+    angle = angle - 45; }
+  
+spinner.setAttribute("style","-webkit-transform: rotateY("+ angle +"deg); -moz-transform: rotateY("+ angle +"deg); transform: rotateY("+ angle +"deg);");
+}
+
+
+
+//============================================================Animation galerie 
