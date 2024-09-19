165caf847ffb02ae419c3e61f9508549983c420c
thomas-pilot-in <thomas@pilot-in.com>
2024-09-19 12:55:10 +0200

    inté: ajout Menu sur App

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import Menu from './components/Menu.vue'
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
+    <Menu></Menu>
+    
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
