de9de04ab816b87c76fd0096b2c759edd59dbfd4
thomas-pilot-in <thomas@pilot-in.com>
2024-09-20 19:10:50 +0200

    init: FooterItem

---
+++ src/components/button/FooterItem.vue
@@ -0,0 +1,24 @@
+<script>
+import { RouterLink } from 'vue-router';
+
+export default {
+    name : "FooterItem",
+    props : {
+        title : String,
+        link : String,
+    },
+    methods: {
+    },
+    components: {
+		RouterLink,
+	},
+ }
+</script>
+
+<template>
+    <RouterLink :to="link" class="footer-item w-fit first:lg:ml-auto after:content-[''] after:w-[0%] after:h-0.5 after:bg-white after:bottom-[-5px] after:left-0 after:absolute after:duration-300 hover:after:w-full font-semibold text-white relative duration-200 ">
+        <span class="content">
+            {{ title }}
+        </span>
+    </RouterLink>
+</template>
