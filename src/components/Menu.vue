12f461b59d83039554d21a1785e4687db835b6e3
thomas-pilot-in <thomas@pilot-in.com>
2024-09-20 11:53:59 +0200

    fix: optimisation du menu et finition js

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,26 +1,108 @@
-<script setup>
-import ButtonPrimary from './button/ButtonPrimary.vue';
-
-</script>
-
 <script>
+import { RouterLink } from 'vue-router';
+import ButtonPrimary from './button/ButtonPrimary.vue';
+import MenuItem from './button/MenuItem.vue';
 export default {
     name : "Menu",
     props : {
     },
     methods: {
+    },
+    components: {
+        ButtonPrimary,
+        MenuItem,
+        RouterLink
     }
 }
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const menu = document.querySelector(".menu");
+    const burger = menu.querySelector(".burger");
+    let didScroll = false;
+    var lastScrollTop = 0;
+    const delta = 10;
+
+    window.addEventListener("scroll", function () {
+        didScroll = true;
+    });
+
+    setInterval(function () {
+        if (didScroll) {
+            hasScrolled();
+            didScroll = false;
+        }
+    }, 160);
+    
+    function hasScrolled() {
+        var st = window.pageYOffset || document.documentElement.scrollTop;
+
+        if (Math.abs(lastScrollTop - st) <= 30) {
+            return;
+        }
+
+        if (st > lastScrollTop && st > menu.getBoundingClientRect().height) {
+            // Scroll Down
+            console.log("down")
+            menu.classList.remove("scroll-default", "scroll-up");
+            menu.classList.add("scroll-down");
+        } else {
+            // Scroll Up
+            console.log("up")
+
+            if (st < menu.getBoundingClientRect().height) {
+                menu.style.setProperty("transition", "0s")
+                menu.classList.remove("scroll-down", "scroll-up");
+                menu.classList.add("scroll-default");
+            } else {
+                menu.style.setProperty("transition", "0.3s ease")
+                menu.classList.remove("scroll-default", "scroll-down");
+                menu.classList.add("scroll-up");
+            }
+        }
+        lastScrollTop = st;
+        console.log(st)
+    }
+
+    menu.classList.add("scroll-default");
+    
+    function handlePageScroll() {
+        if (window.scrollY >= document.innerHeight * 0.6) {
+            menu.classList.add("scroll");
+        } else {
+            menu.classList.remove("scroll");
+        }
+    }
+
+    function handleClickMenu () {
+        let isOpen = menu.dataset.menuIsOpen === "true";
+
+        if(!isOpen ) {
+            menu.dataset.menuIsOpen = "true";
+        } else {
+            menu.dataset.menuIsOpen = "";
+        }
+    }
+    burger.addEventListener("click", handleClickMenu);
+    window.addEventListener("resize", handleClickMenu);
+    window.addEventListener("scroll", handlePageScroll);
+ })
+
+
 </script>
 
 <template>
       <nav class="menu fixed top-0 left-0 w-full z-50 duration-300" >
-            <div class="container px-2">
-                <div class="links flex flex-row gap-4 rounded-b-md items-center py-2 px-4 duration-500">
-                    <a href="/index.html" class="text-[25px] md:text-[33px] font-bold cursor-pointer duration-300 !leading-[160%] hover:pl-8 pl-4 text-white">Thomas</a>
+            <div class="wrapper ">
+                <div class="links flex flex-row gap-4 items-center py-2 px-4 duration-500">
+                    <RouterLink to="/" class="text-[25px] md:text-[33px] font-bold cursor-pointer duration-300 !leading-[160%] hover:pl-8 pl-4 text-white">Thomas</RouterLink>
                    
-                    <div class="nav duration-500 fixed top-[85px] right-0 flex flex-col w-full max-h-[90vh] gap-6 items-center max-lg:bg-black/70 ease-[cubic-bezier(0.53, -0.23, 0.5, 1.16)] max-md:max-w-[95vw] py-12 rounded-l-2xl lg:contents">
-                            <ButtonPrimary title="test" icon="arrow-right" link="/index.html#cta-contact" />                        
+                    <div class="nav duration-500 fixed top-[85px] right-0 flex flex-col w-full max-h-[90vh] gap-6 items-center max-lg:bg-black/70 ease-[cubic-bezier(0.53, -0.23, 0.5, 1.16)] max-md:max-w-[95vw] py-12 rounded-md lg:contents max-lg:backdrop-blur-md max-lg:translate-x-[110%]">
+                            <MenuItem title="Qui suis-je ?" link="/#cta-contact" />                        
+                            <MenuItem title="Réalisations" link="/#cta-contact" />                        
+                            <MenuItem title="Experiences" link="/#cta-contact" />                        
+                            <MenuItem title="Formation" link="/#cta-contact" />                        
+                            <ButtonPrimary title="Contactez moi" icon="arrow-right" link="/#cta-contact" />                        
                     </div>
                     <button class="burger padding-0 relative ml-auto flex cursor-pointer items-center border-none bg-transparent outline-none lg:hidden" aria-label="Main Menu">
                         <svg width="40" height="40" viewBox="0 0 100 100">
@@ -38,69 +120,51 @@ export default {
 <style scoped>
 
 .menu {
-    .links {
-        backdrop-filter: blur(5px);
-        -webkit-backdrop-filter: blur(5px);
-        background: rgba(0, 0, 0, 0.3);
-        &:hover {
-            backdrop-filter: blur(10px);
-            -webkit-backdrop-filter: blur(10px);
-        }
-
-        @media (max-width:1024px) {
-            .nav {
-                backdrop-filter: blur(9px);
-                -webkit-backdrop-filter: blur(9px);
-                transform: translateX(110%);
+    &.scroll-default {
+        @apply lg:absolute;
+        .wrapper {
+            @apply container mx-auto px-2;
+            .links {
+                @apply rounded-b-md backdrop-blur-sm bg-black/30 hover:backdrop-blur-md;
             }
         }
     }
+    &.scroll-up {
+        @apply translate-y-0;
+    }
+    &.scroll-down {
+        @apply -translate-y-full;
+    }
 
+    &.scroll-down,
+    &.scroll-up {
+        @apply fixed ;
+        .wrapper {
+            @apply backdrop-blur-sm bg-black/30 hover:backdrop-blur-md;
+            .links {
+                @apply container mx-auto px-2 ;
+            }
+        }
+    }
     &.scroll {
         .links {
-            background: rgba(0, 0, 0, 0.45);
-        }
-    }
-
-    .btn-menu {
-        &::after {
-            content: "";
-            width: 0%;
-            height: 2px;
-            background: white;
-            bottom: 10px;
-            left: 0;
-            position: absolute;
-            transition: 0.3s;
-        }
-        &:hover {
-            &::after {
-                width: 100%;
-            }
+            @apply bg-black/45;
         }
     }
 
     .burger {
         .line {
-            fill: none;
-            stroke: white;
-            stroke-width: 6;
+            @apply fill-none stroke-white stroke-[6px];
             transition: stroke-dasharray 600ms cubic-bezier(0.4, 0, 0.2, 1),
-                stroke-dashoffset 600ms cubic-bezier(0.4, 0, 0.2, 1);
-
+            stroke-dashoffset 600ms cubic-bezier(0.4, 0, 0.2, 1);
             &.top {
                 stroke-dasharray: 60 207;
-                stroke-width: 6;
             }
-
             &.center {
                 stroke-dasharray: 60 60;
-                stroke-width: 6;
             }
-
             &.bottom {
                 stroke-dasharray: 60 207;
-                stroke-width: 6;
             }
         }
     }
@@ -109,11 +173,9 @@ export default {
     &[data-menu-is-open="true"] {
         @media (max-width:1024px) {
         .links {
-            backdrop-filter: none;
-            -webkit-backdrop-filter: none;
-            background: rgba(0, 0, 0, 0.7);
+            @apply backdrop-blur-none bg-black/70;
                 .nav {
-                    transform: translateX(0);
+                    @apply translate-x-0;
                 }
             }
         }
@@ -123,19 +185,14 @@ export default {
                 &.top {
                     stroke-dasharray: 90 207;
                     stroke-dashoffset: -134;
-                    stroke-width: 6;
                 }
-
                 &.center {
                     stroke-dasharray: 1 60;
                     stroke-dashoffset: -30;
-                    stroke-width: 6;
                 }
-
                 &.bottom {
                     stroke-dasharray: 90 207;
                     stroke-dashoffset: -134;
-                    stroke-width: 6;
                 }
             }
         }
