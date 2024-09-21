d42370ec41b87989bd7b9bfca69d72599ac0a23d
thomas-pilot-in <thomas@pilot-in.com>
2024-09-21 19:59:26 +0200

    fix: ajout placeholer icon du ButtonSecondary

--- src/components/button/ButtonSecondary.vue
+++ src/components/button/ButtonSecondary.vue
@@ -24,14 +24,11 @@ export default {
         </span>
         <span class="icon group-hover:bg-primary-900 group-hover:text-primary-200 bg-primary-200 text-primary-900 size-[30px] p-[7px] overflow-hidden rounded-full duration-300">
             <svg viewBox="0 0 40 16" class="h-4 w-10 fill-current transition-all duration-200 translate-x-[-1.5rem] group-hover:translate-x-0">
-                <use :href="'/src/assets/sprite/far.svg#' + icon" width="16" />
-                <use :href="'/src/assets/sprite/far.svg#' + icon" width="16" x="24" />
+                <use v-if="!icon" :href="'/src/assets/sprite/far.svg#arrow-right'" width="16" />
+                <use v-if="!icon" :href="'/src/assets/sprite/far.svg#arrow-right'" width="16" x="24" />
+                <use v-if="icon" :href="'/src/assets/sprite/far.svg#' + icon" width="16" />
+                <use v-if="icon" :href="'/src/assets/sprite/far.svg#' + icon" width="16" x="24" />
             </svg>
         </span>
     </RouterLink>
 </template>
-
-
-<style>
-
-</style>
