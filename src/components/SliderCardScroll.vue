5cce9e7ab78886527142999facaed165a3a1dac5
thomas-pilot-in <thomas@pilot-in.com>
2024-09-21 21:16:21 +0200

    inté: clean du bouton dans SliderCardScroll

--- src/components/SliderCardScroll.vue
+++ src/components/SliderCardScroll.vue
@@ -44,127 +44,116 @@ export default {
 
 
 <template>
-
-<section class="slider-realisations flex flex-col gap-10 lg:mt-20 py-8 md:py-16 fade-in">
-            <div class="container md:px-12">
-                <div class="intro text-center">
-                    <h2 class="title-4">
-                        {{title}}
-                    </h2>
-                </div>
+    <section class="slider-realisations flex flex-col gap-10 lg:mt-20 py-8 md:py-16 fade-in">
+        <div class="container md:px-12">
+            <div class="intro text-center">
+                <h2 class="title-4">
+                    {{title}}
+                </h2>
             </div>
-            <Splide :options="splideOptions" class="splide">
-                <SplideSlide 
-                    v-for="(slide, index) in slides" 
-                    :key="index" 
-                    class="splide__slide card lg:shadow-2xl group p-6 rounded-2xl h-[400px] lg:h-[20vw] overflow-clip flex flex-col justify-end"
-                >
-                    <img class="image-scroll absolute object-cover inset-0 w-full h-full z-0" :src="slide" alt="">
-                </SplideSlide>
-            </Splide>
-            <div class="container">
-                <div class="outro flex justify-center">
-                    <ButtonSecondary link="/realisations/" title="Voir tout" class="btn-primary group w-fit py-[10px] px-[19px] font-semibold text-base rounded-full relative overflow-hidden select-none touch-manipulation transition-all duration-300 inline-flex items-center gap-2.5 outline outline-primary-900 outline-0 hover:outline-1">
-                        <span class="content">
-                            Voir tout
-                        </span>
-                        <span class="icon  size-[30px] p-[7px] overflow-hidden rounded-full duration-300">
-                            <svg viewBox="0 0 40 16" class="h-4 w-10 fill-current transition-all duration-300 translate-x-[-1.5rem] group-hover:translate-x-0">
-                                <use xlink:href="/src/assets/sprite/far.svg#arrow-right" width="16" />
-                                <use xlink:href="/src/assets/sprite/far.svg#arrow-right" width="16" x="24" />
-                            </svg>
-                        </span>
-                    </ButtonSecondary>
-                </div>
+        </div>
+        <Splide :options="splideOptions" class="splide">
+            <SplideSlide 
+                v-for="(slide, index) in slides" 
+                :key="index" 
+                class="splide__slide card lg:shadow-2xl group p-6 rounded-2xl h-[400px] lg:h-[20vw] overflow-clip flex flex-col justify-end"
+            >
+                <img class="image-scroll absolute object-cover inset-0 w-full h-full z-0" :src="slide" alt="">
+            </SplideSlide>
+        </Splide>
+        <div class="container">
+            <div class="outro flex justify-center">
+                <ButtonSecondary link="/" title="Voir tout" />
             </div>
-        </section>
+        </div>
+    </section>
 </template>
 
 
 <style>
     .slider-realisations {
-    view-timeline-name:--card-scroll-anim;
+        view-timeline-name:--card-scroll-anim;
 
-    .splide__track {
-        @apply !overflow-x-clip !overflow-y-visible;
-    } 
-    .splide__list {
-        @apply lg:!grid grid-cols-5 lg:gap-4 lg:w-[120vw] lg:-translate-x-[10vw];
-    } 
+        .splide__track {
+            @apply !overflow-x-clip !overflow-y-visible;
+        } 
+        .splide__list {
+            @apply lg:!grid grid-cols-5 lg:gap-4 lg:w-[120vw] lg:-translate-x-[10vw];
+        } 
 
-    @supports (animation-timeline: view(block)) {
-        @media (min-width:768px) {
-            .intro {
-                animation-name: intro-anim;
-                animation-timing-function: linear;
-                animation-fill-mode: both;
-                animation-timeline: --card-scroll-anim;
-                animation-duration: 1ms;
-                animation-range: cover 0% cover 60%;
-            }
-            .card {
-                &:nth-child(1),
-                &:nth-child(5) {
-                    animation-name: card-scroll-anim-1;
+        @supports (animation-timeline: view(block)) {
+            @media (min-width:768px) {
+                .intro {
+                    animation-name: intro-anim;
                     animation-timing-function: linear;
                     animation-fill-mode: both;
                     animation-timeline: --card-scroll-anim;
                     animation-duration: 1ms;
-                    animation-range: entry-crossing 0% cover 60%;
+                    animation-range: cover 0% cover 60%;
                 }
-                &:nth-child(2),
-                &:nth-child(4) {
-                    animation-name: card-scroll-anim-2;
-                    animation-timing-function: linear;
-                    animation-fill-mode: both;
-                    animation-timeline: --card-scroll-anim;
-                    animation-duration: 1ms;
-                    animation-range: entry-crossing 0% cover 60%;
-                }
-                &:nth-child(3) {
-                    animation-name: card-scroll-anim-3;
-                    animation-timing-function: linear;
-                    animation-fill-mode: both;
-                    animation-timeline: --card-scroll-anim;
-                    animation-duration: 1ms;
-                    animation-range: entry-crossing 0% cover 60%;
+                .card {
+                    &:nth-child(1),
+                    &:nth-child(5) {
+                        animation-name: card-scroll-anim-1;
+                        animation-timing-function: linear;
+                        animation-fill-mode: both;
+                        animation-timeline: --card-scroll-anim;
+                        animation-duration: 1ms;
+                        animation-range: entry-crossing 0% cover 60%;
+                    }
+                    &:nth-child(2),
+                    &:nth-child(4) {
+                        animation-name: card-scroll-anim-2;
+                        animation-timing-function: linear;
+                        animation-fill-mode: both;
+                        animation-timeline: --card-scroll-anim;
+                        animation-duration: 1ms;
+                        animation-range: entry-crossing 0% cover 60%;
+                    }
+                    &:nth-child(3) {
+                        animation-name: card-scroll-anim-3;
+                        animation-timing-function: linear;
+                        animation-fill-mode: both;
+                        animation-timeline: --card-scroll-anim;
+                        animation-duration: 1ms;
+                        animation-range: entry-crossing 0% cover 60%;
+                    }
                 }
             }
         }
+        
     }
-    
-}
 
-@keyframes intro-anim {
-    from {
-        transform: translateY(-150px);
+    @keyframes intro-anim {
+        from {
+            transform: translateY(-150px);
+        }
+        to {
+            transform: translateY(0%);
+        }
     }
-    to {
-        transform: translateY(0%);
+    @keyframes card-scroll-anim-1 {
+        from {
+            transform: translateY(-250px);
+        }
+        to {
+            transform: translateY(0%);
+        }
     }
-}
-@keyframes card-scroll-anim-1 {
-    from {
-        transform: translateY(-250px);
+    @keyframes card-scroll-anim-2 {
+        from {
+            transform: translateY(-150px);
+        }
+        to {
+            transform: translateY(0%);
+        }
     }
-    to {
-        transform: translateY(0%);
+    @keyframes card-scroll-anim-3 {
+        from {
+            transform: translateY(-70px);
+        }
+        to {
+            transform: translateY(0%);
+        }
     }
-}
-@keyframes card-scroll-anim-2 {
-    from {
-        transform: translateY(-150px);
-    }
-    to {
-        transform: translateY(0%);
-    }
-}
-@keyframes card-scroll-anim-3 {
-    from {
-        transform: translateY(-70px);
-    }
-    to {
-        transform: translateY(0%);
-    }
-}
 </style>
