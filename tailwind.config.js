4c589a8c328506736cd2ee1d71fbc5c6609b7ed1
thomas-pilot-in <thomas@pilot-in.com>
2024-09-21 15:13:01 +0200

    fix: modif tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,11 @@ export default {
         "primary-200": '#81b6f2',
         "primary-600": '#233A5F',
         "primary-900": '#252773',
-        "primary-gradient": "linear-gradient(240deg, rgba(84,58,183,1) 0%, rgba(0,172,193,1) 100%)",
         "damier": '#6d9fbe',
+      },
+      background: {
+        "primary-gradient": "linear-gradient(240deg, rgba(84,58,183,1) 0%, rgba(0,172,193,1) 100%)",
+        "primary-gradient-40": "linear-gradient(60deg, rgba(84,58,183,0.1) 0%, rgba(0,172,193,0.1) 100%)",
       },
       fontFamily: {
         "primary": 'Mustica Pro',
