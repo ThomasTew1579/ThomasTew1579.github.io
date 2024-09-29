1a06380a8205427beda3314549739fa3ed15be9f
thomas-pilot-in <thomas@pilot-in.com>
2024-09-29 15:15:54 +0200

    inté: WrapperAbout et DamierAbout dans AboutView

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,56 +1,68 @@
 <script>
 import HeroImage from '../components/HeroImage.vue';
-import Damier from '../components/Damier.vue';
+import DamierAbout from '../components/DamierAbout.vue';
+import WrapperAbout from '../components/WrapperAbout.vue';
 
 export default {
     name : "HomeView",
     components:{
       HeroImage,
-      Damier
+      DamierAbout,
+      WrapperAbout
     },
 }
 </script>
 
 <template>
   <HeroImage title="Qui suis-je ?" image="/image/blue.jpg" />
-  <Damier title="Musique" :order = "true" image="/image/about/musique.webp" >
-    Avec plus de 15 ans d’expérience en guitare et basse, et 10 ans en tant que chanteur, la musique a toujours été une source d’inspiration et d’expression pour moi. J'ai joué dans un groupe de reprises punk rock, avec plusieurs performances scéniques à mon actif. Cette expérience m'a appris à collaborer efficacement au sein d’équipes créatives et à gérer des projets collectifs, des compétences que je transpose également dans la gestion de projets web. Mon objectif actuel est d’apprendre le violon pour interpréter des bandes originales de films et de jeux vidéo, ce qui reflète ma quête constante de maîtrise technique et créative.
 
-  </Damier>
-  <Damier title="Concerts" :order = "false" image="/image/about/sport.webp" >
-    Depuis 8 ans, je travaille dans l’organisation de concerts, principalement dans les genres punk rock et metalcore, mais également pour d'autres styles musicaux variés. Je suis spécialisé dans la gestion technique, particulièrement la lumière et le backline. Ces événements, souvent complexes, demandent une planification rigoureuse et une gestion technique précise, des compétences que j’applique également dans le développement web pour assurer la performance et la qualité des sites que je développe.
+  <div class="container mx-auto max-w-[1000px] flex flex-col gap-4 text-center">
+    <h2 class="title-3 fade-in">
+      Thomas Pasquet
+    </h2>
+    <p class="text-center fade-in">
+      Je suis développeur web à Lyon. Passionné par la création sous toutes ses formes, j’ai un parcours varié allant de la musique à la modélisation 3D, en passant par l’organisation de concerts et la photographie. Ces expériences nourrissent mon travail en développement web, alliant technique et créativité. Découvrez-en plus sur mes passions et mon parcours.
+    </p>
 
-  </Damier>
-  <Damier title="Clip vidéo" :order = "true" image="/image/about/paint.webp" >
-    En tant qu’éclairagiste, caméraman, monteur et décorateur, j’ai réalisé une dizaine de clips musicaux pour divers groupes lyonnais. Cette expérience m'a permis de développer une approche technique approfondie, ainsi qu'une sensibilité artistique, que j’applique également dans mon métier de développeur web, où l’esthétique et la technique doivent s’harmoniser pour offrir une expérience utilisateur de qualité.
+  </div>
+  <WrapperAbout>
+    <DamierAbout title="Musique" :order = "true" image="/image/about/musique.webp" >
+      Avec plus de 15 ans d’expérience en guitare et basse, et 10 ans en tant que chanteur, la musique a toujours été une source d’inspiration et d’expression pour moi. J'ai joué dans un groupe de reprises punk rock, avec plusieurs performances scéniques à mon actif. Cette expérience m'a appris à collaborer efficacement au sein d’équipes créatives et à gérer des projets collectifs. Mon objectif actuel est d’apprendre le violon pour interpréter des bandes originales de films et de jeux vidéo, ce qui reflète ma quête constante de maîtrise technique et créative.
 
-  </Damier>
-  <Damier title="Photographie" :order = "false" image="/image/about/modelism.webp" >
-    Depuis 4 ans, je me passionne pour la photographie, notamment dans le domaine du sport et des concerts. J'utilise des longues focales pour capturer le mouvement avec précision, une approche qui demande autant de rigueur que de créativité. Tout comme dans mes projets photographiques, je recherche dans mes créations web des interfaces dynamiques et réactives qui capturent l’attention de l’utilisateur tout en assurant une fluidité optimale.
-  </Damier>
-  <Damier title="Peinture" :order = "true" image="/image/about/menuiserie.webp" >
-    Depuis trois ans, je développe mon style artistique en peinture, en me concentrant sur des logos et des illustrations monochromes. J’ai réalisé plusieurs planches de skate personnalisées pour des groupes de musique et peint deux guitares, ainsi que quelques tableaux pour mon appartement. Ce travail exige patience, précision et un sens aigu de l'esthétique, des qualités que je mets également à profit dans le développement de sites web sur mesure, où l’équilibre entre design et fonctionnalité est essentiel.
+    </DamierAbout>
 
-  </Damier>
-  <Damier title="Modélisme" :order = "false" image="/image/about/live.webp" >
-    Depuis mon enfance, je construis des voitures télécommandées, principalement des buggys tout terrain motorisés. Aujourd'hui, j'envisage de m'orienter vers la construction de modèles d’avions et de drones. Cette passion pour le modélisme m’a appris à porter attention aux détails mécaniques et à la précision, des compétences qui trouvent une application directe dans le développement front-end, où chaque ligne de code doit être méticuleusement optimisée pour garantir la performance du site.
+    <!-- <DamierAbout title="Concerts" :order = "false" image="/image/about/sport.webp" >
+      Depuis 8 ans, je travaille dans l’organisation de concerts, principalement dans les genres punk rock et metalcore, mais également pour d'autres styles musicaux variés. Je suis spécialisé dans la gestion technique, particulièrement la lumière et le backline. Ces événements, souvent complexes, demandent une planification rigoureuse et une gestion technique précise, des compétences que j’applique également dans le développement web pour assurer la performance et la qualité des sites que je développe.
+    </DamierAbout> -->
 
-  </Damier>
-  <Damier title="VModélisation 3D" :order = "true" image="/image/about/js.webp" >
-    Depuis 6 ans, je pratique la modélisation 3D, en particulier pour la conception de meubles, de bâtiments et de véhicules. J’utilise principalement Sketchup Pro et 3ds Max pour créer des conceptions détaillées, comme la réplique en 3D du Palais de la Bourse de Lyon. La modélisation 3D et le développement web partagent une même exigence de précision et d’attention aux détails, que je maîtrise pour concevoir des projets fonctionnels et esthétiques.
+    <DamierAbout title="Photographie" :order = "false" image="/image/about/modelism.webp" >
+      Depuis 4 ans, je me passionne pour la photographie, notamment dans le domaine du sport et des concerts. J'utilise des longues focales pour capturer le mouvement avec précision, une approche qui demande autant de rigueur que de créativité. Tout comme dans mes projets photographiques, je recherche dans mes créations web des interfaces dynamiques et réactives qui capturent l’attention de l’utilisateur tout en assurant une fluidité optimale.
+    </DamierAbout>
 
-</Damier>
-<Damier title="Rétro-gaming" :order = "false" image="/image/about/jv.webp" >
-  Ma passion pour le rétro-gaming, avec des jeux comme Spyro, Tomb Raider et Final Fantasy sur PS1, ainsi que Pokémon sur Game Boy, est motivée par mon admiration pour le design graphique et la nostalgie des jeux classiques. Je collectionne les consoles Sony et les portables de toutes marques. Cette appréciation pour le design se reflète dans ma manière d’aborder le développement web, où j’aime combiner l’esthétique moderne avec des éléments de design intemporels.
+    <DamierAbout title="Clip vidéo" :order = "true" image="/image/about/paint.webp" >
+      En tant qu’éclairagiste, caméraman, monteur et décorateur, j’ai réalisé une dizaine de clips musicaux pour divers groupes lyonnais. Cette expérience m'a permis de développer une approche technique approfondie, ainsi qu'une sensibilité artistique, que j’applique également dans mon métier de développeur web, où l’esthétique et la technique doivent s’harmoniser pour offrir une expérience utilisateur de qualité.
+    </DamierAbout>
 
-  </Damier>
-  <Damier title="Jeux vidéo" :order = "true" image="/image/about/js.webp" >
-    Les jeux d’énigmes comme Tunic et Outer Wilds me passionnent pour leur capacité à stimuler la réflexion et à proposer des défis complexes. Je peux passer des heures à résoudre des énigmes, une démarche qui s’apparente à ma manière d’aborder les problèmes dans le développement web. Tout comme dans les jeux vidéo, je cherche à décoder et optimiser les interactions utilisateurs, en tirant parti de ma capacité à repérer les subtilités et à explorer des solutions créatives.
+    <DamierAbout title="Peinture" :order = "false" image="/image/about/menuiserie.webp" >
+      Depuis trois ans, je développe mon style artistique en peinture, en me concentrant sur des logos et des illustrations monochromes. J’ai réalisé plusieurs planches de skate personnalisées pour des groupes de musique et peint deux guitares, ainsi que quelques tableaux pour mon appartement. Ce travail exige patience, précision et un sens aigu de l'esthétique, des qualités que je mets également à profit dans le développement de sites web sur mesure, où l’équilibre entre design et fonctionnalité est essentiel.
 
-  </Damier>
-  <Damier title="Sport" :order = "false" image="/image/about/js.webp" >
-    Les sports que je pratique, comme l’escalade, le skateboard et la randonnée, sont des moyens pour moi de repousser mes limites tout en appréciant l’environnement naturel. Je pratique le skateboard quotidiennement, principalement comme moyen de transport en ville, et participe à des rassemblements de skate où j’interviens aussi en tant que photographe ou organisateur. Cette recherche d'équilibre entre technique et performance est également présente dans ma manière de gérer des projets complexes en développement web, où chaque détail compte.
+    </DamierAbout>
+    <!-- <DamierAbout title="Modélisme" :order = "false" image="/image/about/live.webp" >
+      Depuis mon enfance, je construis des voitures télécommandées, principalement des buggys tout terrain motorisés. Aujourd'hui, j'envisage de m'orienter vers la construction de modèles d’avions et de drones. Cette passion pour le modélisme m’a appris à porter attention aux détails mécaniques et à la précision, des compétences qui trouvent une application directe dans le développement front-end, où chaque ligne de code doit être méticuleusement optimisée pour garantir la performance du site.
+    </DamierAbout> -->
+    <DamierAbout title="Modélisation 3D" :order = "true" image="/image/about/js.webp" >
+      Depuis 6 ans, je pratique la modélisation 3D, en particulier pour la conception de meubles, de bâtiments et de véhicules. J’utilise principalement Sketchup Pro et 3ds Max pour créer des conceptions détaillées, comme la réplique en 3D du Palais de la Bourse de Lyon. La modélisation 3D et le développement web partagent une même exigence de précision et d’attention aux détails, que je maîtrise pour concevoir des projets fonctionnels et esthétiques.
 
-  </Damier>
+  </DamierAbout>
+  <!-- <DamierAbout title="Rétro-gaming" :order = "false" image="/image/about/jv.webp" >
+    Ma passion pour le rétro-gaming, avec des jeux comme Spyro, Tomb Raider et Final Fantasy sur PS1, ainsi que Pokémon sur Game Boy, est motivée par mon admiration pour le design graphique et la nostalgie des jeux classiques. Je collectionne les consoles Sony et les portables de toutes marques. Cette appréciation pour le design se reflète dans ma manière d’aborder le développement web, où j’aime combiner l’esthétique moderne avec des éléments de design intemporels.
+    </DamierAbout> -->
+    <DamierAbout title="Jeux vidéo" :order = "false" image="/image/about/js.webp" >
+      Les jeux d’énigmes comme Tunic et Outer Wilds me passionnent pour leur capacité à stimuler la réflexion et à proposer des défis complexes. Je peux passer des heures à résoudre des énigmes, une démarche qui s’apparente à ma manière d’aborder les problèmes dans le développement web. Tout comme dans les jeux vidéo, je cherche à décoder et optimiser les interactions utilisateurs, en tirant parti de ma capacité à repérer les subtilités et à explorer des solutions créatives.
+    </DamierAbout>
+    <DamierAbout title="Sport" :order = "true" image="/image/about/js.webp" >
+      Les sports que je pratique, comme l’escalade, le skateboard et la randonnée, sont des moyens pour moi de repousser mes limites tout en appréciant l’environnement naturel. Je pratique le skateboard quotidiennement, principalement comme moyen de transport en ville, et participe à des rassemblements de skate où j’interviens aussi en tant que photographe ou organisateur. Cette recherche d'équilibre entre technique et performance est également présente dans ma manière de gérer des projets complexes en développement web, où chaque détail compte.
+    </DamierAbout>
+  </WrapperAbout>
 </template>
 
