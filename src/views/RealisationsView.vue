7a0f597a36dcc9b0dae6fe41996377155251af96
thomas-pilot-in <thomas@pilot-in.com>
2024-10-02 21:32:40 +0200

    inté: ArchiveRealisation sur RealisationView

--- src/views/RealisationsView.vue
+++ src/views/RealisationsView.vue
@@ -1,12 +1,14 @@
 <script>
 import HeroImage from '../components/HeroImage.vue';
 import Damier from '../components/Damier.vue';
+import ArchiveRealisations from '../components/ArchiveRealisations.vue';
 
 export default {
     name : "RealisationsView",
     components:{
       HeroImage,
       Damier,
+      ArchiveRealisations,
     },
 }
 </script>
@@ -14,9 +16,7 @@ export default {
 <template>
   <HeroImage title="Réalisations" image="/image/illustrations/illu-real.webp" />
 
-  <Damier title="Modélisation 3D" :order = "true" image="/image/photo-perso.jpg" >
-    https://www.biens-surs.fr/
+  <ArchiveRealisations />
 
-  </Damier>
 </template>
 
