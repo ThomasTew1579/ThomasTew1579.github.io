e073b5a84fd6f99d30d4bd2f936629468937dc96
thomas-pilot-in <thomas@pilot-in.com>
2024-10-18 14:05:26 +0200

    inté: archive réalisation

--- src/components/ArchiveRealisations.vue
+++ src/components/ArchiveRealisations.vue
@@ -14,58 +14,58 @@ export default {
             content: [
             {
                 name:"Inser-eco93 ",
-                text:"Sur le projet Inser-eco93, j’ai pris en charge le front-end et le back-end, en travaillant seul pour développer un site efficace et optimisé. En utilisant WordPress, PHP, CSS et JavaScript, j’ai assuré une navigation fluide et des performances techniques solides. Ce projet, mené à bien sans difficulté particulière, a permis de livrer un site parfaitement fonctionnel et en accord avec les attentes du client, qui s'est montré satisfait du travail réalisé.",
+                text:"Développement front-end et back-end pour un site vitrine. Projet réalisé seul avec satisfaction du client. Ce projet m'a permis d’appliquer mon expertise pour créer un site fonctionnel et ergonomique.",
                 media:"/image/screen/insereco.webp",
               },
               {
-                url:"https://urps-mk-ara.com/",
-                name:"urps-mk-ara",
-                text:"",
+                url:"https://urps-mk-ara.org/",
+                name:"URPS MK ARA",
+                text:"Création d’un filtre d’archives et d’une carte interactive en front-end et back-end. Client très satisfait du résultat. Cette solution technique complexe a amélioré considérablement l'expérience utilisateur en facilitant la recherche d’informations.",
                 media:"/image/screen/urps-mk-ara.webp",
               },
               {
                 url:"https://pilot-in.com/",
-                name:"pilot-in",
-                text:"",
+                name:"Pilot'in",
+                text:"Premier gros projet full-stack réalisé seul. Le client a été satisfait du développement front-end et back-end. Cette réalisation a marqué une étape importante dans ma carrière en consolidant mes compétences techniques et ma gestion de projets d'envergure.",
                 media:"/image/screen/pilotin.webp",
               },
               {
                 url:"https://neoloc-immobilier.fr/",
                 name:"Neoloc",
-                text:"Pour Neoloc, une agence de gestion locative à Lyon, j’ai été chargé de la refonte complète du site sans maquette, ce qui m’a donné l’opportunité d’utiliser pleinement mes compétences créatives et techniques. En tant que développeur full-stack, j’ai pris en main à la fois le front-end et le back-end, en utilisant WordPress, PHP, CSS et JavaScript. Le résultat final a amélioré l’expérience utilisateur et optimisé la navigation du site, avec des retours très positifs du client, satisfait par ma capacité à travailler en autonomie.                ",
+                text:"Refonte totale du site vitrine en front-end et back-end sans maquette. Résultat satisfaisant, le client continue de collaborer. J’ai su proposer des solutions efficaces pour répondre aux attentes du client tout en offrant une navigation optimisée.",
                 media:"/image/screen/neoloc.webp",
               },
               {
-                url:"https://novely.com/",
+                url:"https://novely-renovation.fr/",
                 name:"Novely",
-                text:"",
+                text:"Refonte complète du site vitrine sans maquette, en front-end et back-end. Client satisfait du travail réalisé. J’ai pris des décisions créatives et techniques pour moderniser le site et améliorer son attractivité.",
                 media:"/image/screen/novely.webp",
               },
               {
                 name:"Serpol",
-                text:"Sur le projet Serpol, branche du groupe Serfim, j’ai pris en charge l’intégralité du développement, aussi bien en front-end qu’en back-end. Ce projet m’a permis d’exploiter toute la gamme des technologies WordPress, PHP, CSS et JavaScript pour développer une solution complète. Travaillant seul, j’ai pu apporter une réponse technique aux besoins du client et garantir un développement optimisé du site. Même si le site n'est pas encore en ligne, les retours préliminaires du client sont extrêmement positifs.",
+                text:"Développement complet front-end et back-end pour Serpol, branche de Serfim. Projet terminé avec succès. Client satisfait. Le site est conçu pour être évolutif et adaptable aux besoins futurs de l’entreprise.",
                 media:"/image/screen/serpol.webp",
               },
               {
                 name:"Serfim TIC",
-                text:"Dans le cadre de Serfim TIC, également une branche du groupe Serfim, j’ai été chargé de la conception front-end et back-end du site web. Ce projet, bien que technique, s’est déroulé sans embûches majeures. En utilisant WordPress, PHP, CSS et JavaScript, j’ai su proposer une solution à la fois fonctionnelle et durable. Le client a été satisfait des performances du site et de sa facilité d’utilisation, ce qui témoigne de mon expertise en gestion de projets complexes.",
+                text:"Développement front-end et back-end. Site structuré et performant livré avec satisfaction du client. Ce projet a renforcé ma capacité à concevoir des solutions stables pour des entreprises à forte exigence technique.",
                 media:"/image/screen/serfimtic.webp",
               },
               {
                 name:"Terenvie",
-                text:"Pour Ternie, une autre branche du groupe Serfim, j’ai assuré la gestion complète du développement front-end et back-end. Travaillant de manière indépendante sur ce projet, j’ai déployé des solutions basées sur WordPress, PHP, CSS et JavaScript pour garantir un site performant et sécurisé. Le projet a été finalisé avec succès et a pleinement répondu aux attentes du client, renforçant ma capacité à livrer des projets dans les délais et avec une grande autonomie.",
+                text:"Développement front-end et back-end pour cette branche de Serfim. Résultats très positifs et client satisfait. J’ai assuré un développement fluide et respecté les délais tout en garantissant une qualité optimale.",
                 media:"/image/screen/terrenvie.webp",
               },
               {
-                url:"https://inovaya.com/",
+                url:"https://www.pournosenfants.com/",
                 name:"MTB pour nos enfants",
-                text:"Le site MTB pour nos enfants m’a offert l’occasion de me concentrer sur le développement d’un site dynamique comportant un grand nombre d’animations au scroll. Travaillant sur le front-end et le back-end, j’ai utilisé WordPress, PHP, CSS et JavaScript pour créer une interface interactive et engageante. Le défi consistait à intégrer ces animations sans affecter les performances du site, ce que j'ai réalisé avec succès. Le client a été ravi du résultat final, qui reflétait bien les attentes initiales.",
+                text:"Développement full-stack avec intégration d’animations au scroll. Le client a apprécié l'interactivité du site. Les animations apportent une touche dynamique tout en garantissant une navigation fluide.",
                 media:"/image/screen/mtbpne.webp",
               },
               {
-                url:"https://urps-mk-ara.org/",
-                name:"manerty ",
-                text:"Pour le projet URPS MK ARA, j'ai développé un système de filtrage d’archives avancé, associé à une carte interactive. En tant que développeur full-stack, j'ai utilisé WordPress, PHP, CSS et JavaScript pour intégrer des fonctionnalités complexes tout en maintenant une navigation fluide et intuitive. Ce projet a exigé une expertise technique spécifique pour répondre aux besoins du client, qui s'est montré très satisfait du résultat final et de la fluidité de l'expérience utilisateur.",
+                url:"https://www.manerty.com/",
+                name:"Manerty ",
+                text:"Premier site vitrine full-stack que j’ai développé seul. Le client est satisfait et nous travaillons toujours ensemble pour faire évoluer le site. Ce projet m’a permis de consolider mes compétences en front-end et back-end tout en répondant aux exigences spécifiques du client.",
                 media:"/image/screen/manerty.webp",
               },
               
@@ -78,91 +78,69 @@ export default {
               {
                 url:"https://inovaya.com/",
                 name:"Inovaya",
-                text:"En tant que développeur front-end au sein d'une équipe pour le projet Inovaya, j’ai travaillé sur l’intégration d'animations avancées au scroll. Ce projet m’a permis de collaborer avec d’autres experts pour assurer la cohérence entre design et technique, tout en utilisant WordPress, PHP, CSS et JavaScript. Les défis techniques autour de ces animations ont été relevés avec succès, et le client a apprécié la qualité et la fluidité des interactions proposées.",
+                text:"Développement d’animations au scroll en front-end, en équipe. Le client a été satisfait du rendu interactif. Le site offre une expérience visuelle fluide, parfaitement alignée avec les attentes du client.",
                 media:"/image/screen/inovaya.webp",
               },
               {
-                url:"https://cabinet-eyssette.com/",
+                url:"https://www.cabinet-eyssette.fr/",
                 name:"Cabinet Eyssette",
-                text:"",
+                text:"Développement des animations au scroll en CSS et du style du site en front-end. Projet réalisé en équipe avec succès. Client satisfait. J’ai contribué à créer une expérience utilisateur fluide grâce à des animations élégantes et efficaces.",
                 media:"/image/screen/cabinet-eyssette.webp",
               },
               {
-                url:"https://serfim.com/",
+                url:"https://www.serfim.com/",
                 name:"Serfim",
-                text:"",
+                text:"Développement des animations et du style du site en front-end. Ce projet m’a permis d'apprendre le back-end sur WordPress. Client satisfait. J’ai su m'adapter rapidement aux besoins techniques pour offrir un site fonctionnel et attractif.",
                 media:"/image/screen/serfim.webp",
               },
               {
-                url:"https://inovaya.com/",
+                url:"https://katrimmo.com/",
                 name:"Katrimmo",
-                text:"Pour Katrimmo, une agence de location immobilière, ma mission principale était de repenser entièrement le style du site, avec une attention particulière portée à la version mobile. En tant que développeur front-end, j’ai redéfini l’apparence du site, optimisé son accessibilité mobile et utilisé WordPress, PHP, CSS et JavaScript pour obtenir une meilleure performance. Ce projet a renforcé mon expertise en conception responsive et a permis au client de bénéficier d’un site plus moderne et fonctionnel, adapté à leurs besoins.",
+                text:"Refonte du style mobile en front-end, réalisée seul. Le site est optimisé pour une meilleure expérience utilisateur. Client satisfait. Ce projet m'a permis de perfectionner mes compétences en conception mobile-first.",
                 media:"/image/screen/kattrimo.webp",
               },
               {
-                url:"https://dott.com/",
+                url:"https://ridedott.com/fr/",
                 name:"Dott",
-                text:"",
+                text:"Refonte complète du style du site et intégration en 8 langues. J’ai respecté un design strict imposé par le client. Le projet a été un succès, et je continue de collaborer pour les évolutions. Cette collaboration continue témoigne de la confiance que le client accorde à mon travail.",
                 media:"/image/screen/dott.webp",
               },
               {
                 url:"https://www.sepr.edu/",
                 name:"SEPR",
-                text:"Pour SEPR, un projet complexe avec un grand nombre d’animations, j’ai été impliqué en tant que développeur front-end au sein d'une équipe. J'ai utilisé WordPress, PHP, CSS et JavaScript pour implémenter ces animations tout en maintenant une performance optimale. Ce projet a mis en avant ma capacité à gérer de nombreux éléments interactifs sans nuire à l’expérience utilisateur. Le client a été très satisfait du rendu final, à la fois fonctionnel et esthétique.",
+                text:"Projet en équipe avec développement front-end de nombreuses animations. Le client a exprimé sa satisfaction. Nous avons réussi à maintenir une performance élevée malgré la complexité des animations.",
                 media:"/image/screen/sepr.webp",
               },
               {
-                url:"https://ltdln.com/",
-                name:"ltdln",
-                text:"",
+                url:"https://latetedanslesnuages.com/",
+                name:"La Tête dans les Nuages",
+                text:"Développement des animations et du style avec de nombreuses micro-animations en front-end, en équipe. Résultat apprécié par le client. Ce projet a mis en valeur ma capacité à créer des interfaces interactives et ludiques, adaptées à l'univers des jeux d'arcade.",
                 media:"/image/screen/ltdln.webp",
               },
               {
-                url:"https://inovaya.com/",
+                url:"https://www.biens-surs.fr/",
                 name:"Biens Sûrs",
-                text:"Pour Biens Sûrs, promoteur immobilier, j’ai collaboré au sein d'une équipe pour le développement front-end du site, en utilisant WordPress, PHP, CSS et JavaScript. Ce projet visait à concevoir un site moderne et intuitif, avec une attention particulière portée à l’ergonomie et à l’expérience utilisateur. Grâce à une collaboration efficace, nous avons livré un site répondant parfaitement aux attentes du client, qui a été très satisfait du résultat.",
+                text:"Développement front-end en équipe pour un promoteur immobilier. Le client a apprécié le design et la performance du site. Ce projet a permis d’allier esthétique et fonctionnalité pour une expérience utilisateur optimale.",
                 media:"/image/screen/manerty.webp",
               },
               {
                 url:"https://thermiconseil.com/",
                 name:"Thermiconseil",
-                text:"",
+                text:"Projet en équipe pour le développement front-end, avec des défis de mise en page de contenu dense. Le client est satisfait. J’ai su trouver des solutions créatives pour rendre le contenu technique plus accessible et lisible.",
                 media:"/image/screen/thermiconseil.webp",
               },
               {
-                url:"https://bienssurs.org/",
-                name:"bienssurs",
-                text:"",
-                media:"/image/screen/bienssurs.webp",
+                url:"https://www.groupe-sra.fr/",
+                name:"SRA Group",
+                text:"Développement front-end en équipe, avec une grande diversité de styles et de contenu. Client satisfait du rendu final. J’ai contribué à optimiser l'organisation des contenus pour une navigation fluide et intuitive.",
+                media:"/image/screen/sra.webp",
               },
               {
                 url:"https://unijus.org/",
                 name:"Unijus",
-                text:"",
+                text:"Premier projet en front-end avec développement des animations et du style. Résultat final validé par le client. Ce projet m’a permis de faire mes premières armes en animation web tout en respectant les exigences de l’industrie.",
                 media:"/image/screen/unijus.webp",
               },
-            ]
-          },
-          {
-            type:"regisseur",
-            typeName:"Evenementiel",
-            content: [
-              {
-                name:"test",
-                text:"text",
-                media:"/image/fox.svg",
-              }
-            ]
-          },
-          {
-            type:"clip",
-            typeName:"Tournage vidéo",
-            content: [
-              {
-                name:"test",
-                text:"text",
-                media:"/image/fox.svg",
-              }
             ]
           },
         ]
@@ -177,7 +155,7 @@ export default {
     <section class="archive-ralisations">
         <div class="container mx-auto flex flex-col lg:flex-row gap-12">
             <div class="sidebar">
-                <div class="content sticky flex flex-col gap-4 duration-300">
+                <div class="content sticky flex flex-col gap-4 px-4 duration-300">
                   <strong class="text-xl">Type de projet :</strong>
                   <a v-for="(realisation, index) in realisations" :key="index" :href="'#'+realisation.type" class="types px-4 py-2 rounded-sm bg-primary-100 hover:text-white hover:bg-primary-900 duration-200 flex group overflow-hidden whitespace-nowrap pr-12 flex-row items-center gap-2">
                     {{ realisation.typeName }}
@@ -187,7 +165,7 @@ export default {
                   </a>
                 </div>
             </div>
-            <div class="realisations flex flex-col gap-10">
+            <div class="realisations flex flex-col gap-10 max-md:px-4">
                 <WrapperRealisationsType :realisations="realisations" />
             </div>
         </div>
